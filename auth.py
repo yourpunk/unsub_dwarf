87cf9a6afe69563ee2c79f6c42ef32c7d8a2a89a
Aleksandra <aleks.creatrix@gmail.com>
2025-04-05 22:13:16 +0200

    Add start files

---
+++ auth.py
@@ -0,0 +1,30 @@
+import os.path
+import pickle
+import google.auth.transport.requests
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+def get_gmail_service():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(google.auth.transport.requests.Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+if __name__ == "__main__":
+    service = get_gmail_service()
+    print("Authorization successful!")
